8bc50896b8ef2ef250583354b89cc79af1228b81
Sergio David Medina Mateus <smedina09101124@gmail.com>
2021-01-30 20:38:33 -0500

    variables_constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,25 @@
+var nombre = "Sergio";
+let edad  =  33;
+var profesor = false;
+let lista_cursos = ['JavaScript', 'unity', 'html'];
+var informacion_personal = {
+    id: 1098644869,
+    direccion: "calle 12 No. 12 - 15",
+    lista_cursos2 : ['JavaScript', 'unity', 'html']
+};
+
+
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacion_personal);
+
+
+const cedula = 123456;
+console.log(cedula);
+const cedula2 = 88888;
+console.log(cedula2);
+
+console.warn(cedula);
